02552305b5accefb285247fbbdb618187843fcd7
M-C Pronovost <mc@nixa.ca>
2025-07-24 08:52:18 -0400

    Update index.html title and root element, remove unused CSS configuration, and add new SCSS files for styling

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,11 +8,4 @@ export default defineConfig({
     // watch: { usePolling: true },
   },
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: "@import \"src/styles/variables.scss\"; @import \"src/styles/mixins.scss\";"
-      }
-    }
-  },
 });
