5b04614d698889ed710fd68d240d57e1b3003b76
M-C Pronovost <mc@nixa.ca>
2025-07-28 16:53:45 -0400

    Add path aliasing in Vite configuration for easier imports

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -12,5 +13,10 @@ export default defineConfig(({ mode }) => {
       watch: { usePolling: watchUsePolling },
     },
     plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
   };
 });
