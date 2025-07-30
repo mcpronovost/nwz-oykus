91e79e1d24d911ab696c052f47de3622664736aa
M-C Pronovost <mc@nixa.ca>
2025-07-30 15:12:08 -0400

    Add translation context and utility functions

---
+++ frontend/src/services/translation/index.jsx
@@ -0,0 +1,54 @@
+import React, { createContext, useContext, useEffect, useRef } from "react";
+import { DEFAULT_LANG, loadTranslations } from "./utils";
+
+const TranslationContext = createContext();
+
+export function TranslationProvider({ children, lang = DEFAULT_LANG }) {
+  const translationRef = useRef(null);
+
+  useEffect(() => {
+    const translation = loadTranslations(lang);
+    translationRef.current = translation;
+  }, [lang]);
+
+  const handleTranslate = (key, count) => {
+    if (!translationRef.current) {
+      const translation = loadTranslations(lang);
+      translationRef.current = translation;
+    }
+
+    const translation = translationRef.current[key];
+    if (typeof translation === "object" && count !== undefined) {
+      const rules = new Intl.PluralRules(lang);
+      const pluralForm = rules.select(count);
+      return translation[pluralForm] || translation["other"];
+    }
+
+    if (typeof translation === "object") {
+      return translation["other"];
+    }
+
+    if (typeof translation === "string") {
+      return translation;
+    }
+
+    // eslint-disable-next-line no-console
+    console.warn(`Translation not found for key: "${key}".`);
+    return key;
+  };
+
+  const value = {
+    lang,
+    t: handleTranslate,
+  };
+
+  return (
+    <TranslationContext.Provider value={value}>
+      {children}
+    </TranslationContext.Provider>
+  );
+}
+
+export function useTranslation() {
+  return useContext(TranslationContext);
+}
