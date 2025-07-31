d118e748d180317dfb8e55ffb256d8673913bbbe
M-C Pronovost <mc@nixa.ca>
2025-07-31 13:27:44 -0400

    Remove ProtectedRoute component from common exports

--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -4,4 +4,3 @@ export { default as Dropdown } from "./Dropdown.jsx";
 export { default as Heading } from "./Heading.jsx";
 export { default as Link } from "./Link.jsx";
 export { default as Modal } from "./Modal.jsx";
-export { default as ProtectedRoute } from "./ProtectedRoute.jsx";
