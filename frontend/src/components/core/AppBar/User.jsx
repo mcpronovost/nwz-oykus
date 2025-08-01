fb470da3e5413141f449eb408338101196a5e64c
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-07-31 20:51:39 -0400

    Update AppBarUser component to include additional menu items with translations; modify logout handling; adjust popper item background colour in styles

--- frontend/src/components/core/AppBar/User.jsx
+++ frontend/src/components/core/AppBar/User.jsx
@@ -1,21 +1,20 @@
-import { User } from "lucide-react";
+import { User, LogOut, ListTodo, Orbit, Users } from "lucide-react";
 
 import { api } from "@/services/api";
 import { useRouter } from "@/services/router";
 import { useStore } from "@/services/store";
+import { useTranslation } from "@/services/translation";
 import { Dropdown } from "@/components/common";
 
 export default function AppBarUser() {
   const { n } = useRouter();
   const { currentUser } = useStore();
+  const { t } = useTranslation();
 
   const handleLogout = async () => {
     try {
       await api.logout();
-      n("login");
-    } catch (error) {
-      console.error("Logout error:", error);
-      // Even if server logout fails, clear local data and redirect
+    } finally {
       n("login");
     }
   };
@@ -33,7 +32,36 @@ export default function AppBarUser() {
       }
       menu={[
         {
-          label: "Logout",
+          label: t("Your profile"),
+          onClick: () => {},
+        },
+        {
+          divider: true,
+        },
+        {
+          label: t("Your worlds"),
+          icon: <Orbit size={18} />,
+          disabled: true,
+          onClick: () => {},
+        },
+        {
+          label: t("Your characters"),
+          icon: <Users size={18} />,
+          disabled: true,
+          onClick: () => {},
+        },
+        {
+          label: t("Your tasks"),
+          icon: <ListTodo size={18} />,
+          disabled: true,
+          onClick: () => {},
+        },
+        {
+          divider: true,
+        },
+        {
+          label: t("Logout"),
+          icon: <LogOut size={18} />,
           onClick: handleLogout,
         },
       ]}
