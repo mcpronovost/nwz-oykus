e17978f84655414888194ce3773a8dcdecfc0bd9
M-C Pronovost <mc@nixa.ca>
2025-08-06 11:20:24 -0400

    Refactor AppBarMenu to use OykDropdown and OykLink for mobile menu

--- frontend/src/components/core/AppBar/Menu.jsx
+++ frontend/src/components/core/AppBar/Menu.jsx
@@ -1,24 +1,48 @@
-import { useTranslation } from "@/services/translation";
+import { Menu } from "lucide-react";
 
-import { Link } from "@/components/common";
+import { useRouter } from "@/services/router";
+import { useTranslation } from "@/services/translation";
+import { OykButton, OykDropdown, OykLink } from "@/components/common";
 
 export default function AppBarMenu() {
+  const { n } = useRouter();
   const { t } = useTranslation();
+
+  const menuList = [
+    {
+      label: t("About"),
+      routeName: "about",
+      onClick: () => n("about"),
+    },
+    {
+      label: t("Privacy Policy"),
+      routeName: "privacy-policy",
+      onClick: () => n("privacy-policy"),
+    },
+  ];
 
   return (
     <section className="oyk-app-bar-menu">
+      <div className="oyk-app-bar-menu-mobile">
+        <OykDropdown
+          toggle={
+            <OykButton className="oyk-app-bar-menu-mobile-toggle" plain style={{ padding: 0 }}>
+              <Menu size={24} />
+            </OykButton>
+          }
+          menu={menuList}
+          direction="right"
+        />
+      </div>
       <nav className="oyk-app-bar-menu-nav">
         <ul className="oyk-app-bar-menu-nav-list">
-          <li className="oyk-app-bar-menu-nav-item">
-            <Link routeName="about" className="oyk-app-bar-menu-nav-item-link">
-              {t("About")}
-            </Link>
-          </li>
-          <li className="oyk-app-bar-menu-nav-item">
-            <Link routeName="privacy-policy" className="oyk-app-bar-menu-nav-item-link">
-              {t("Privacy Policy")}
-            </Link>
-          </li>
+          {menuList.map((item) => (
+            <li key={item.label} className="oyk-app-bar-menu-nav-item">
+              <OykLink routeName={item.routeName} className="oyk-app-bar-menu-nav-item-link">
+                {item.label}
+              </OykLink>
+            </li>
+          ))}
         </ul>
       </nav>
     </section>
